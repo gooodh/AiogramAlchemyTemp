85c2291c25434b10f2bfa0fc6a0ddc920739b07f
Nikulin <s.b.nikuliny@gmail.com>
2025-02-14 13:42:13 +0700

    feat: added bot_utils.py

--- bot/main.py
+++ bot/main.py
@@ -1,60 +1,18 @@
 import asyncio
 import tracemalloc
-
 from aiohttp import web
 from loguru import logger
-
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiogram.types import BotCommand, BotCommandScopeDefault
-
-
-from dao.database_middleware import DatabaseMiddlewareWithCommit, DatabaseMiddlewareWithoutCommit
-from config import BASE_URL, HOST, PORT, WEBHOOK_PATH, bot, admins, dp
-
+from config import BASE_URL, HOST, PORT, WEBHOOK_PATH, bot, dp
 from users.router import user_router
-
-async def set_commands():
-    commands = [
-        BotCommand(command="start", description="Старт"),
-
-    ]
-    await bot.set_my_commands(commands, BotCommandScopeDefault())
-
-
-async def start_bot():
-    await set_commands()
-    try:
-        for admin_id in admins:
-            await bot.send_message(admin_id, "Я запущен🥳.")
-
-        response = await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
-        if response:
-            logger.info(f"Вебхук успешно установлен: {response}.")
-        else:
-            logger.warning(f"Вебхук не установлен, ответ пуст: {response}.")
-    except Exception as e:
-        logger.error(f"Ошибка при установке вебхука: {e}")
-    logger.info(f"Сервер запущен на {HOST}:{PORT}")
-
-
-async def stop_bot():
-    try:
-        for admin_id in admins:
-            await bot.send_message(admin_id, "Бот остановлен. За что?😔")
-        await bot.delete_webhook(drop_pending_updates=True)
-        await bot.session.close()
-    except Exception as e:
-        logger.error(e)
-    logger.error("Бот остановлен!")
+from bot_utils import start_bot, stop_bot, register_middlewares
 
 
 async def main():
     tracemalloc.start()
 
-        # регистрация мидлварей
-    dp.update.middleware.register(DatabaseMiddlewareWithoutCommit())
-    dp.update.middleware.register(DatabaseMiddlewareWithCommit())
-    # регистрация роутеров
+    # Регистрация middleware и роутеров
+    register_middlewares(dp)  # Используем функцию для регистрации middleware
     dp.include_router(user_router)
 
     app = web.Application()
@@ -62,26 +20,36 @@ async def main():
     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
     setup_application(app, dp, bot=bot)
 
-    # Запускаем веб-сервер на указанном хосте и порте
+    # Установка вебхука и логирование результата
+    try:
+        response = await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
+        if response:
+            logger.info(f"Вебхук успешно установлен: {response}.")
+        else:
+            logger.warning("Вебхук не установлен, ответ пуст.")
+    except Exception as e:
+        logger.error(f"Ошибка при установке вебхука: {e}")
+
+    # Запускаем веб-сервер
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, host=HOST, port=PORT)
     await site.start()
 
-    # Бесконечный цикл, чтобы сервер работал
     try:
+        await start_bot()
         while True:
             await asyncio.sleep(3600)  # Ждем 1 час
     except KeyboardInterrupt:
-        pass  # Позволяет остановить сервер с помощью Ctrl+C
+        pass
     finally:
-        # Здесь можно добавить код для завершения соединений или других ресурсов
-        await runner.cleanup()  # Закрываем веб-сервер
-        print("Ресурсы очищены.")
+        await stop_bot()
+        await runner.cleanup()
+        logger.info("Ресурсы очищены.")
 
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Программа завершена пользователем.")
+        logger.info("Программа завершена пользователем.")
