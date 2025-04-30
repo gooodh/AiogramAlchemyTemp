38ebfeda1f3c62c4e0d15785817f665b896d2db2
Nikulin <s.b.nikuliny@gmail.com>
2025-04-30 11:19:30 +0700

    fix: added delete_webhook

--- bot/main_pulling.py
+++ bot/main_pulling.py
@@ -16,6 +16,7 @@ async def main():
 
     # Запуск бота в режиме long polling
     try:
+        await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
     finally:
         await bot.session.close()
